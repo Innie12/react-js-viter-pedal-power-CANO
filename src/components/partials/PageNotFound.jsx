43cd6cefa2d5fa88f3824332d33c807cf7146f23
bullets012 <lancegenesismercado0111@gmail.com>
2025-07-16 08:19:02 +0800

    commit

--- src/components/partials/PageNotFound.jsx
+++ src/components/partials/PageNotFound.jsx
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom';
 const PageNotFound = () => {
   return (
     <>
-      <div className="text-center mt-64 ">
-        <h1 className="text-9xl mb-3 font-bold text-gray-700">404</h1>
-        <p className="mb-10 text-3xl">Oppss...This Page Not Found!</p>
-        <Link
-          to="/"
-          className="inline-block underline text-black font-large transition transform hover:scale-105"
-        >
-          Back to Home
-        </Link>
+      <div className="mx-auto py-20">
+        <div className="justify-items-center">
+          <img src="./images/sadness.webp" alt="picture of sadness crying" />
+        </div>
+        <div className="text-center">
+          <h1 className="text-9xl mb-3 font-bold text-gray-700">404</h1>
+          <p className="mb-10 text-3xl">Oppss...This Page Not Found!</p>
+          <Link
+            to="/"
+            className="inline-block underline text-gray-900 text-xl transition transform hover:scale-105"
+          >
+            Back to Home
+          </Link>
+        </div>
       </div>
     </>
   );
